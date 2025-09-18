6fec5da6cf3827c8a0416787a1467c3c95f7e9d5
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-09-18 12:10:24 -0400

    League sign in stuff

--- lambdas/user_login/user_data.py
+++ lambdas/user_login/user_data.py
@@ -18,7 +18,7 @@ async def login_user(data: dict) -> dict:
             return {}
         elif user['password'] == password:
             log.info("User password matches. Validating user in Sleeper League.")
-            sleeper_user = get_sleeper_user(user_id)
+            sleeper_user = await get_sleeper_user(user_id)
             league = get_item_by_key(LEAGUE_DATA_TABLE_NAME, 'league_id', league_id)
             if sleeper_user['id'] in league['user_ids']:
                 log.info("User is in league. Login Successful.")
